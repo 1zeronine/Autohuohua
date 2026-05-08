45efb6a8219a9a01d7368133dfe64fe3765e5175
1zeronine <2351486599@qq.com>
2026-05-08 11:58:38 +0800

    修复定时任务执行失败: 添加完整 node 路径和日志记录

--- run.bat
+++ run.bat
@@ -1,5 +1,14 @@
 @echo off
 chcp 65001 >nul
 cd /d D:\Autohuohua
-node index.js
-pause
+
+:: Log start
+echo [%date% %time%] 开始执行 >> run.log 2>&1
+
+:: Ensure node is in PATH
+set PATH=C:\Program Files\nodejs;%PATH%
+
+:: Run the script, capture output to log
+node index.js >> run.log 2>&1
+
+echo [%date% %time%] 执行结束 >> run.log 2>&1
